fd7a8bcb957372e0e06a40bd2f9cab4feaec08d6
Connor Kendrick <connorkendrickmusic@gmail.com>
2018-10-31 21:19:20 -0400

    Connect better-sqlite3 database

--- bot.js
+++ bot.js
@@ -2,6 +2,8 @@ const Discord = require('discord.js');
 const { createLogger, format, transports} = require('winston');
 const auth = require('./auth.json');
 const client = new Discord.Client();
+const SQLite = require('better-sqlite3');
+const sql = new SQLite('./mappings.sqlite');
 
 // Configure logger settings
 const logger = createLogger({
@@ -14,6 +16,22 @@ const logger = createLogger({
 client.on('ready', () => {
   logger.info('Connected');
   logger.info(`Logged in as ${client.user.tag}!`);
+
+  // Check if table "map" exists
+  const table = sql.prepare('SELECT count(*) FROM sqlite_master WHERE type=\'table\' AND name=\'maps\';').get();
+  if (!table['count(*)']) {
+    // If the table isn't there, create it and setup the database correctly
+    // id is the concatenation of the guildId and the emoji (so unique id can exist)
+    sql.prepare('CREATE TABLE maps (id TEXT PRIMARY KEY, guildId TEXT, emoji TEXT, url TEXT, startTime TEXT, endTime TEXT);').run();
+    sql.pragma('synchronous=1');
+    sql.pragma('journal_mode=wal');
+  }
+
+  // Two prepared statements to get and set data
+  client.getGuildMaps = sql.prepare('SELECT * FROM maps WHERE guildId=?');
+  client.getMap = sql.prepare('SELECT * FROM maps WHERE id=?');
+  client.addMap = sql.prepare('INSERT OR REPLACE INTO maps (id, guildId, emoji, url, startTime, endTime) VALUES (@id, @guildId, @emoji, @url, @startTime, @endTime);');
+  client.removeMap = sql.prepare('DELETE FROM maps WHERE id=?');
 });
 
 // Send intro message on guild join
@@ -38,10 +56,13 @@ client.on('message', msg => {
 
   // Bot should react differently when being messaged in a guild vs. in a DM
   if (msg.guild) {
-    // Voice channel of user who sent message
-    const voiceChannel = msg.member.voiceChannel;
     // Use to differentiate emoji mappings for different servers
     let guildId = msg.guild.id;
+    // Specific emoji to sound clip mapping
+    // TODO cache in local memory so it doesn't have to query the database every msg?
+    let map = client.getMap.get(guildId + msg.content);
+    // Voice channel of user who sent message
+    const voiceChannel = msg.member.voiceChannel;
 
     // Bot listens for messages that start with `!` for commands
     if (msg.content.substring(0, 1) === '!') {
@@ -52,6 +73,8 @@ client.on('message', msg => {
         // Create new mapping of emoji and sound clip for server
         case 'otoadd':
           msg.channel.send('Command to map a sound to an emoji');
+          // create map object, m
+          // client.addMap.run(m);
           break;
         // Remove mapping of emoji and sound clip for server
         case 'otoremove':
@@ -76,8 +99,7 @@ client.on('message', msg => {
       msg.channel.send(helpMessage);
     }
 
-    // TODO: Iterate through list of keys for server in database. If match found, play sound clip
-    if (msg.content === 'emoji' && voiceChannel) {
+    if (map && voiceChannel && msg.content === map.emoji) {
       voiceChannel.join();
       setTimeout(() => {
         voiceChannel.leave();
